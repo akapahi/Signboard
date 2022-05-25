5c140e3bec9d3d83c9ae15dd0224b8406c3bc929
Abhishek Kapahi <54096646+akapahi@users.noreply.github.com>
2022-05-25 12:12:50 +0530

    fixed bg.js

--- public/bg.js
+++ public/bg.js
@@ -3,6 +3,14 @@ function setup() {
 	canvas.position(0,0);
 	canvas.style('z-index','-1')
 	background(240);
+	getJSON('/users',
+	function(err, data) {
+  	if (err !== null) {
+    		alert('Something went wrong: ' + err);
+  	} else {
+    		alert('Your query count: ' + data.query.count);
+  	}
+});
 }
 
 function windowResized(){
@@ -13,3 +21,18 @@ function windowResized(){
 function draw() {
 	background(240);
 }
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
