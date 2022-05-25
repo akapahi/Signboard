cea08852b65a51dcc6fbed3199da352e804285a8
Abhishek Kapahi <54096646+akapahi@users.noreply.github.com>
2022-05-25 15:13:10 +0530

    fixed subtact func

--- public/bg.js
+++ public/bg.js
@@ -48,7 +48,7 @@ class scrollText{
 	}
 
 	update() {
-		this.position.subtract(this.velocity);
+		this.position.sub(this.velocity);
 		if (this.position.x > width) this.position.x = 0;
 		if (this.position.x < 0) this.position.x = width;
 		if (this.position.y > height) this.position.y = 0;
