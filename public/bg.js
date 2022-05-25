3fd1006a162ac01b2f43b35d1e672a05849ac644
Abhishek Kapahi <54096646+akapahi@users.noreply.github.com>
2022-05-25 12:14:57 +0530

    logged data

--- public/bg.js
+++ public/bg.js
@@ -8,7 +8,7 @@ function setup() {
   	if (err !== null) {
     		alert('Something went wrong: ' + err);
   	} else {
-    		alert('Your query count: ' + data.query.count);
+    		alert('Your query count: ' + data);
   	}
 });
 }
