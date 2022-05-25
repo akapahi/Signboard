5c84dc477df34007d5bc2e3c463d45f085340f97
Abhishek Kapahi <54096646+akapahi@users.noreply.github.com>
2022-05-25 12:07:19 +0530

    added fetch to sketch

---
+++ views/bg.js
@@ -0,0 +1,38 @@
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0,0);
+	canvas.style('z-index','-1')
+	background(240);
+	getJSON('/users',
+	function(err, data) {
+  	if (err !== null) {
+    		alert('Something went wrong: ' + err);
+  	} else {
+    		alert('Your query count: ' + data.query.count);
+  	}
+});
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight)	
+}
+
+
+function draw() {
+	background(240);
+}
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
